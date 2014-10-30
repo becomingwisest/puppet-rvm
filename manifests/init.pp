28c9f47fbdd6b8cba667cdd8b6c819d37ff1c077
Christopher Evans <christopher.evans@reachlocal.com>
2014-10-30 14:27:12 -0700

    fixup

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,11 @@ class rvm($version='latest', $install_rvm=true) {
   stage { 'rvm-install': before => Stage['main'] }
 
   if $install_rvm {
-    exec{'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3':} ->
+    exec{'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3':}
     class {
       'rvm::dependencies': stage => 'rvm-install';
       'rvm::system':       stage => 'rvm-install', version => $version;
     }
+    Exec['gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3'] -> Class['rvm::dependencies']
   }
 }
