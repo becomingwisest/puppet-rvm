176048e795c2627479842b963999b4c019a0687b
Christopher Evans <christopher.evans@reachlocal.com>
2014-10-30 14:22:28 -0700

    Change where we install the rvm key.

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -1,5 +1,4 @@
 class rvm::dependencies {
-  exec{'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3':} ->
   case $::operatingsystem {
     Ubuntu,Debian: { require rvm::dependencies::ubuntu }
     CentOS,RedHat,Amazon: { require rvm::dependencies::centos }
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@ class rvm($version='latest', $install_rvm=true) {
   stage { 'rvm-install': before => Stage['main'] }
 
   if $install_rvm {
+    exec{'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3':} ->
     class {
       'rvm::dependencies': stage => 'rvm-install';
       'rvm::system':       stage => 'rvm-install', version => $version;
