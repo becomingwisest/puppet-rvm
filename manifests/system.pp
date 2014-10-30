da4747f0d93192959746e24cc4209d19f57b5a49
Christopher Evans <christopher.evans@reachlocal.com>
2014-10-30 12:45:32 -0700

    Adding --trace

--- manifests/system.pp
+++ manifests/system.pp
@@ -3,7 +3,7 @@ class rvm::system($version='latest') {
     path    => '/usr/bin:/usr/sbin:/bin',
     command => "bash -c '/usr/bin/curl -sSL https://get.rvm.io -o /tmp/rvm-installer ; \
                 chmod +x /tmp/rvm-installer ; \
-                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${version} ; \
+                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${version} --trace ; \
                 rm /tmp/rvm-installer'",
     creates => '/usr/local/rvm/bin/rvm',
     require => [
