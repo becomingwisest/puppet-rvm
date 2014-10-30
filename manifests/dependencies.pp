e19f4e7f42b6fd0b381e7d57d0135ce571ce607c
Christopher Evans <christopher.evans@reachlocal.com>
2014-10-30 14:08:12 -0700

    Install the gpg key.

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -1,4 +1,5 @@
 class rvm::dependencies {
+  exec{'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3':} ->
   case $::operatingsystem {
     Ubuntu,Debian: { require rvm::dependencies::ubuntu }
     CentOS,RedHat,Amazon: { require rvm::dependencies::centos }
